61ef0b7c8b5990e4bcdef51a262c7a1f57d956dd
Eve Keoy <eve.keoy@infrontconsulting.com.my>
2025-01-02 13:07:46 +0800

    separated auth script

--- services/apiServices.js
+++ services/apiServices.js
@@ -1,43 +1,9 @@
+const { getAccessToken } = require('./auth');
 const axios = require("axios");
 require('dotenv').config();
 
 const BASE_URL = process.env.MOLI_BASE_URL;
 const ACCOUNT_BASE_URL = process.env.ACCOUNT_BASE_URL;
-const AUTH_TOKEN_URL = process.env.AUTH_TOKEN_URL;
-const AUTH_CLIENT_ID = process.env.AUTH_CLIENT_ID;
-const AUTH_CLIENT_SECRET = process.env.AUTH_CLIENT_SECRET;
-
-let ACCESS_TOKEN = process.env.ACCESS_TOKEN;
-let ACCESS_TOKEN_EXPIRY_TIME = process.env.ACCESS_TOKEN_EXPIRY_TIME;
-  
-  const getAccessToken = async () => {
-    const currentTime = new Date().getTime();
-    if (ACCESS_TOKEN && currentTime < ACCESS_TOKEN_EXPIRY_TIME) {
-      return ACCESS_TOKEN; 
-    }
-  
-    try {
-      const response = await axios.post(AUTH_TOKEN_URL, null, {
-        params: {
-          grant_type: "client_credentials",
-          client_id: AUTH_CLIENT_ID,
-          client_secret: AUTH_CLIENT_SECRET,
-        },
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          "Accept": "*/*",
-        },
-      });
-      ACCESS_TOKEN = response.data.access_token;
-      ACCESS_TOKEN_EXPIRY_TIME = currentTime + response.data.expires_in * 1000;
-      process.env.ACCESS_TOKEN = ACCESS_TOKEN;
-      process.env.ACCESS_TOKEN_EXPIRY_TIME = ACCESS_TOKEN_EXPIRY_TIME;
-      return ACCESS_TOKEN;
-    } catch (error) {
-      console.error("Error fetching access token:", error);
-      throw new Error("Failed to get access token");
-    }
-  };
 
 const fetchApiStatus = async (msisdn, telco, id) => {
   const results = { msisdn , telco, id};
